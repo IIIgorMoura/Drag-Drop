eeb482bfbf7c6a60b91a4964c40c78df1d2a6f3b
IIIgorMoura <igor2007moura@gmail.com>
2023-10-09 11:12:15 -0300

    a

--- projeto/main.js
+++ projeto/main.js
@@ -1,51 +1,65 @@
-const tarefas = document.querySelectorAll('.tarefas');
-const colunas = document.querySelectorAll('.coluna');
+const tarefas = document.querySelectorAll('.tarefa');
+        const colunas = document.querySelectorAll('.coluna');
 
-//var pra armazen tarefa sendo arrastada
-let tarefaArrastada = null;
+        // Variável para armazenar a tarefa sendo arrastada
+        let tarefaArrastada = null;
 
-tarefas.forEach(tarefa => {
-    tarefa.addEventListener('dragstart');
+        tarefas.forEach(tarefa => {
+            tarefa.addEventListener('dragstart', iniciarArrasto);
+            tarefa.addEventListener('dragend', finalizarArrasto);
+        });
 
-    tarefa.addEventListener('dragend', finalizarArrasto);
-});
+        colunas.forEach(coluna => {
+            coluna.addEventListener('dragover', permitirSoltar);
+            coluna.addEventListener('drop', soltarTarefa);
+        });
 
-colunas.forEach(coluna => {
-    coluna.addEventListener('dragover', permitirSoltar);
+        function iniciarArrasto(event) {
+            // Funcao para iniciar o arrasto da tarefa
+            tarefaArrastada = this;
 
-    coluna.addEventListener('drop', soltarTarefa);
-});
+            // Define o efeito visual do arrastar
+            this.classList.add('tarefa-arrastando');
+        }
 
-function iniciarArrasto(event){
-    // Funcao pra iniciar arrasto da tarefa
-    tarefaArrastada = this;
+        function finalizarArrasto(event) {
+            this.classList.remove('tarefa-arrastando');
+        }
 
-    // Define o efeito visual do arrastar
-    this.classList.add('tarefa-arrastando');
-};
+        function permitirSoltar(event) {
+            // Permitir soltar tarefa na coluna
+            event.preventDefault();
+        }
 
-function finalizarArrasto(event){
-    this.classList.remove('tarefa-arrastando');
-};
+        function soltarTarefa(event) {
+            // Permitir soltar tarefa na coluna
+            event.preventDefault();
 
-function permitirSoltar(event){
-    // permitir soltar tarefa na coluna
-    event.preventDefault();
-};
+            if (tarefaArrastada) {
+                this.querySelector('ul').appendChild(tarefaArrastada);
+                tarefaArrastada = null;
+            }
+        }
 
-function soltarTarefa(event){
-    // permitir soltar tarefa na coluna
-    event.preventDefault();
-};
+        const formAdicionarTarefa = document.getElementById('adicionar-tarefa');
 
-if(tarefaArrastada){
-    this.querySelector('.tarefas').appendChild(tarefaArrastada);
-    tarefaArrastada = null;
-};
+        formAdicionarTarefa.addEventListener('submit', adicionarTarefa);
 
+        function adicionarTarefa(event) {
+            event.preventDefault();
 
+            const novaTarefa = document.getElementById('nova-tarefa').value;
+            if (novaTarefa) {
+                const novaTarefaElemento = document.createElement('li');
 
+                novaTarefaElemento.innerHTML = novaTarefa;
+                novaTarefaElemento.draggable = true;
+                novaTarefaElemento.classList.add('tarefa');
 
-const formAdicionarTarefa = document.getElementById('adicionar-tarefa');
+                document.getElementById('tarefa-fazer').appendChild(novaTarefaElemento);
+                document.getElementById('nova-tarefa').value = '';
 
-formAdicionarTarefa.addEventListener('submit', adicionarTarefa)
+                novaTarefaElemento.addEventListener('dragstart', iniciarArrasto);
+                novaTarefaElemento.addEventListener('dragend', finalizarArrasto);
+            }
+        }
